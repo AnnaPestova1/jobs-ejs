84009ff09525e4344b6ff7762d43a46d796918cf
Anna Pestova <106414157+AnnaPestova1@users.noreply.github.com>
2024-02-15 10:31:00 -0500

    fix issues

--- controllers/sessionController.js
+++ controllers/sessionController.js
@@ -29,22 +29,22 @@ const registerShow = (req, res) => {
 
 const registerDo = async (req, res, next) => {
   let validation_errors = false;
-  if (req.body.password != req.body.password1) {
-    // req.flash("error", "The passwords entered do not match.");
-    return res.render("register", { errors: req.flash("error") });
-  }
-
-  try {
-    await User.create(req.body);
-  } catch (e) {
-    console.error(e);
-    if (e.constructor.name === "ValidationError") {
-      parse_v(e, req);
-    } else if (e.name === "MongoServerError" && e.code === 11000) {
-      req.flash("error", "That email address is already registered.");
-    } else {
-      return next(e);
+  if (req.body.password === req.body.password1) {
+    try {
+      await User.create(req.body);
+    } catch (e) {
+      console.error(e);
+      if (e.constructor.name === "ValidationError") {
+        parse_v(e, req);
+      } else if (e.name === "MongoServerError" && e.code === 11000) {
+        req.flash("error", "That email address is already registered.");
+      } else {
+        return next(e);
+      }
+      return res.render("register", { errors: req.flash("error") });
     }
+  } else {
+    // req.flash("error", "The passwords entered do not match.");
     return res.render("register", { errors: req.flash("error") });
   }
 
